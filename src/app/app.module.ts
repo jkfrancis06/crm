7af286d9f6afa243e33cb1cff2621145ab36bda0
jkfrancis06 <jkfrancis06@gmail.com>
2018-09-01 21:27:14 +0000

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,6 @@ import {AuthGuard} from './core/auth.guard';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { NavFooterComponent } from './components/nav-footer/nav-footer.component';
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
-import {AvatarModule} from 'ng2-avatar';
 
 
 
@@ -56,7 +55,6 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    AvatarModule,
     //
     MzValidationModule,
     MzInputModule,
