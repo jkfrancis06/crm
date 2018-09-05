217fc5e2dcd6d7576620ffde66dd623ec8450dd1
jkfrancis06 <jkfrancis06@gmail.com>
2018-09-05 22:53:13 +0000

    commit

--- src/app/providers/const/constant-service.service.ts
+++ src/app/providers/const/constant-service.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class ConstantServiceService {
   // public SERVER_ADDRESS = 'http://197.255.237.253:8001';
   // public SERVER_ADDRESS = 'http://192.168.43.195';
-  public SERVER_ADDRESS = 'http://http://ec2-13-59-236-64.us-east-2.compute.amazonaws.com';
+  public SERVER_ADDRESS = 'http://ec2-13-59-236-64.us-east-2.compute.amazonaws.com';
 
   constructor() { }
 }
